0bcd7617751b88f5feab8ee8ff86a51862d825f5
Gustavo Dominguez <gus.dominguez@duocuc.cl>
2026-07-05 23:09:17 -0400

    fix: minor fixes

--- src/main/java/com/duoc/enrollmentplatform/factory/RabbitMQConfiguration.java
+++ src/main/java/com/duoc/enrollmentplatform/factory/RabbitMQConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,6 +33,6 @@ public class RabbitMQConfiguration {
 
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 }
